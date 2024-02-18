721e74b5560ee85003faae496b88247a57f63ad7
emirsapmaz <21comp1005@isik.edu.tr>
2024-02-18 18:40:36 +0300

    slight change

--- src/ansıcolors/ANSIcolors.java
+++ src/ansıcolors/ANSIcolors.java
@@ -5,6 +5,8 @@
  */
 package ansıcolors;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Emir Sapmaz
@@ -14,8 +16,23 @@ public class ANSIcolors {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws InterruptedException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the text that you want to make colorful!");
+        String coloredText = input.nextLine();
+        String ANSIcolor = "\u001B[30m";
+        String ANSIreset =  "\u001B[0m";
+        int splitted = Integer.parseInt(ANSIcolor.substring(2, 4));//getting the numbers of the ANSI color codes in the string
+        while(true){
+            splitted++; //incremeting the ANSI color code to change the color
+            String newSplitted = Integer.toString(splitted); //converting the incremented number to string to be able to concat it to the orginal string
+            String newString = ANSIcolor.substring(0, 2).concat(newSplitted+"m"); //adding the first part of the string and numbers together to make a new incremented string
+            System.out.println(newString+coloredText+ANSIreset); //printing the colorful text, we need to add the color code in the beginning, text in the middle and reset(if needed) at the end of the string.
+            if(splitted==45) //making the loop go back to the beginning
+                splitted=29;
+            else if(splitted==37) // skipping few numbers because they are colorles.
+                splitted=40;
+            Thread.sleep(400); //making the loop slower by making the main thread sleep 
+        }
     }
-    
 }
